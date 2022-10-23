952f812ff14dfa13d3142ab6b9a71e2be957790d
Decoyenemy <n_gazizov@mail.ru>
2022-10-23 13:01:59 +0300

    добавлено решение задачи 15

---
+++ Lesson01/homework2/hometask15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+    if(day == 6 || day == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день недели");
+    }
