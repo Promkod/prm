9039fca1e7e89a7a685cd1f37b350e035016598e
Decoyenemy <n_gazizov@mail.ru>
2022-10-23 12:56:26 +0300

    добавлено решение 13ой задачи

---
+++ Lesson01/homework2/hometask13/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Console.WriteLine("Введите трехзначное число.");
+string number = Convert.ToString(Convert.ToInt32(Console.ReadLine()));
+if (number.Length > 2)
+{
+    Console.WriteLine(number[2]);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
