29afe3f62cdf52b907285032cceab9840a5acbc4
Decoyenemy <n_gazizov@mail.ru>
2022-10-29 04:07:57 +0300

    добавлено решине 23 задачи

---
+++ Lesson01/homework3/task23/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = ReadInt("Введите число: ");
+int m = 1;
+
+for (m = 1; m <= number; m++)
+{
+    Console.WriteLine($"{Math.Pow(m, 3)}");
+}
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
