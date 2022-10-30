514819959b1980bf6090eacc2b919ce066b9524e
Decoyenemy <n_gazizov@mail.ru>
2022-10-30 12:10:47 +0300

    homework4 added

---
+++ Lesson01/homework4/task25/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberA = ReadInt("Введите число: ");
+int numberB = ReadInt("Введите степень: ");
+ToDegree(numberA, numberB);
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
