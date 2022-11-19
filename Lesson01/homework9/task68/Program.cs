84422747a611e602f4886260aae435912ef70480
Decoyenemy <n_gazizov@mail.ru>
2022-11-19 13:36:25 +0300

    task68 done

---
+++ Lesson01/homework9/task68/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+
+// вызов функции
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akker(m, n)); 
+}
+
+// функция Аккермана
+int Akker(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akker(m - 1, 1);
+    }
+    else
+    {
+        return (Akker(m - 1, Akker(m, n - 1)));
+    }
+}
